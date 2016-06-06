cbb56a6eab2f6018b13aabb94854c4e4fd7a09e6
Vivek Kiran <vivek.kiran@outlook.com>
2016-06-06 17:58:42 +0530

    fix

--- app/src/main/java/com/wavelabs/nfund/About50kFragment.java
+++ app/src/main/java/com/wavelabs/nfund/About50kFragment.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.wavelabs.R;
 import com.wavelabs.nfund.model.AboutUs;
 import com.wavelabs.nfund.service.AppAPI;
 
